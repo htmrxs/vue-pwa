b5be823dde62a2e2e5808b69cae126b9d62ef43d
htmrxs <bthaytam@gmail.com>
2020-12-30 00:03:29 +0100

    changes

---
+++ src/components/FooterInfo.vue
@@ -0,0 +1,20 @@
+<template>
+<footer class="caption">
+  <p>TIP: Double-click to edit a todo</p>
+  <p><a href="https://github.com/htmrxs/vue-pwa" target="_blank" rel="noopener">View on GitHub</a></p>
+</footer>
+</template>
+
+<style lang="stylus">
+footer
+  margin: 65px auto 0
+  color: #777777
+  text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5)
+  p
+    line-height: 1
+  a
+    color: inherit
+    text-decoration: none
+    &:hover
+      text-decoration: underline
+</style>
